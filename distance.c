402eda3f2ade3a410a77224b9f458c61349ca3bf
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-07 13:38:45 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<math.h>
-double x1,y1,x2,y2;
+double x1=0.0;
+double y=0.0;
+double x2=0.0;
+double y2=0.0;
 void p1()
 {
 printf("Enter x coordinate of point 1\n");
 scanf("%lf",&x1);
 printf("Enter y coordinate of point 1\n");
-scanf("%lf",&y1);
+scanf("%lf",&y);
 }
 void p2()
 {
@@ -15,9 +18,9 @@ scanf("%lf",&x2);
 printf("Enter y coordinate of point 2\n");
 scanf("%lf",&y2);
 }
-double distance()
+double distance(double x1,double x2,double y,double y2)
 {
-double dist=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+double dist=sqrt(pow((x2-x1),2)+pow((y2-y),2));
 return dist;
 }
 void display(double distance)
@@ -28,6 +31,6 @@ int main()
 {
 p1();
 p2();
-double d=distance();
+double d=distance(x1,x2,y,y2);
 display(d);
 }
