d646cc57b4fb65ae59afeedfa7e75da0dad50c52
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-07 16:01:14 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,38 @@
-//WAP to find the distance between two points using structures and 4 functions.
+#include<stdio.h>
+#include<math.h>
+struct point
+{
+double x;
+double y;
+}p1,p2;
+void readp1()
+{
+printf("Enter the x coordinate of first point\n");
+scanf("%lf",&p1.x);
+printf("Enter the y coordinate of first point\n");
+scanf("%lf",&p1.y);
+}
+void readp2()
+{
+printf("Enter the x coordinate of Second point\n");
+scanf("%lf",&p2.x);
+printf("Enter the y coordinate of Second point\n");
+scanf("%lf",&p2.y);
+}
+double distance( double,double,double,double)
+{
+double dist=sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
+return dist;
+}
+void display(double distance)
+{
+printf("The distance is %lf \n",distance);
+}
+int main()
+{
+readp1();
+readp2();
+double d=distance(p1.x,p2.x,p2.y,p2.y);
+display(d);
+return 0;
+}
