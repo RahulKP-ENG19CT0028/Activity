a876d0ed370aca33b4582f9f431f2491c2fb6e9f
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-09 16:32:18 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,32 @@
-//WAP to find the sum of n fractions.
+#include<stdio.h>
+struct fraction
+{
+float x;
+float y;
+};
+void add_frac()
+{
+fraction f;
+printf("Enter the number of fractions to be added(2 0r more)\n");
+int n;
+scanf("%d",&n);
+if(n>=2)
+{
+float sum;
+for(int i=1;i<=n;i++)
+{
+printf("Enter the numerator\n");
+scanf("%f",&f.x);
+printf("Enter the denominator\n");
+scanf("%f",&f.y);
+sum=sum+(f.x/f.y);
+}
+printf("The sum is %f",sum);
+}
+else printf("Invalid input\n");
+}
+int main()
+{
+add_frac();
+return 0;
+}
