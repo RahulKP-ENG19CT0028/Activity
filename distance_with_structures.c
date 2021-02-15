5265e8e31a9f13fc00796660c861c0d5f565c4a2
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-15 21:58:37 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,38 +1,42 @@
 #include<stdio.h>
 #include<math.h>
+//Without Global Variables
 struct point
 {
-double x;
-double y;
-}p1,p2;
-void readp1()
+	double x;
+	double y;
+};
+point readp1(point p1)
 {
-printf("Enter the x coordinate of first point\n");
-scanf("%lf",&p1.x);
-printf("Enter the y coordinate of first point\n");
-scanf("%lf",&p1.y);
+	printf("Enter the x coordinate of first point\n");
+	scanf("%lf",&p1.x);
+	printf("Enter the y coordinate of first point\n");
+	scanf("%lf",&p1.y);
+	return p1;
 }
-void readp2()
+point readp2(point p2)
 {
-printf("Enter the x coordinate of Second point\n");
-scanf("%lf",&p2.x);
-printf("Enter the y coordinate of Second point\n");
-scanf("%lf",&p2.y);
+	printf("Enter the x coordinate of Second point\n");
+	scanf("%lf",&p2.x);
+	printf("Enter the y coordinate of Second point\n");
+	scanf("%lf",&p2.y);
+	return p2;
 }
-double distance( double,double,double,double)
+double distance(point p1,point p2)
 {
-double dist=sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
-return dist;
+	double dist=sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
+	return dist;
 }
 void display(double distance)
 {
-printf("The distance is %lf \n",distance);
+	printf("The distance is %lf units\n",distance);
 }
 int main()
 {
-readp1();
-readp2();
-double d=distance(p1.x,p2.x,p2.y,p2.y);
-display(d);
-return 0;
+	point p1,p2;
+	p1=readp1(p1);
+	p2=readp2(p2);
+	double d=distance(p1,p2);
+	display(d);
+	return 0;
 }
