65119a38b3f8f5e098f5c67e56252a09016dea4e
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-16 07:22:36 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
+int input(int s)
+{
+	printf("How many numbers do you want to add?\n");
+  scanf("%d",&s);
+  return s;
+}
+int add(int s)
+{
+  float n,sum=0.0;
+  printf("Enter the numbers\n");
+  for(int i=1;i<=s;i++)
+  {
+    scanf("%f",&n);
+    sum=sum+n;
+  }
+  return sum;
+}
+void display(float sum)
+{
+	printf("The sum is %f\n",sum);
+}
 int main()
 {
-int s;
-printf("How many numbers do you want to add?\n");
-scanf("%d",&s);
-float n,sum=0.0;
-printf("Enter the numbers\n");
-for(int i=1;i<=s;i++)
-{
-scanf("%f",&n);
-sum=sum+n;
-}
-printf("The sum is %f\n",sum);
-return 0;
+	int size;
+	size = input(size);
+	float sum = add(size);
+	display(sum);
 }
