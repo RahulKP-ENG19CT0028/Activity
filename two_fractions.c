6847e5fded9c0fca413b1d49a19fe8c1ec21a20e
RahulKP-ENG19CT0028 <76113863+RahulKP-ENG19CT0028@users.noreply.github.com>
2021-02-16 07:10:07 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,28 +1,57 @@
 #include<stdio.h>
 struct fraction
 {
-float x;
-float y;
-}f1,f2;
-void read()
+	int x;
+	int y;
+};
+fraction read(fraction f)
 {
-printf("Enter the numerator of fraction 1\n");
-scanf("%f",&f1.x);
-printf("Enter the denominator of fraction 1\n");
-scanf("%f",&f1.y);
-printf("Enter the numerator of fraction 2\n");
-scanf("%f",&f2.x);
-printf("Enter the denominator of fraction 2\n");
-scanf("%f",&f2.y);
+	printf("Enter the numerator of fraction \n");
+	scanf("%d",&f.x);
+	printf("Enter the denominator of fraction \n");
+	scanf("%d",&f.y);
+	return f;
 }
-void add(int,int,int,int)
+fraction add(fraction f1,fraction f2)
 {
-float sum = (f1.x/f1.y) + (f2.x/f2.y);
-printf("The sum is %f \n",sum);
+	fraction sum;
+	sum.y = f1.y*f2.y;
+	sum.x = (f1.x*f2.y)+(f2.x*f1.y);
+	return sum;
+}
+fraction simplify(fraction sum)
+{
+	int hcf = 0;
+	int min = 0;
+	if(sum.x<sum.y)
+	{
+		min = sum.x;
+	}
+	else
+	{
+		min = sum.y;
+	}
+	for(int i=1;i<=min;i++)
+	{
+		if(sum.x%i == 0 && sum.y%i == 0)
+		{
+			hcf = i;
+		}
+	}
+	sum.x = sum.x/hcf;
+	sum.y = sum.y/hcf;
+	return sum;
+}
+void display(fraction res,fraction f1, fraction f2)
+{
+	printf("The sum of %d/%d and %d/%d is %d/%d",f1.x,f1.y,f2.x,f2.y,res.x,res.y);
 }
 int main()
-{
-read();
-add(f1.x,f1.y,f2.x,f2.y);
-return 0;
+{	
+	fraction f1,f2;
+	f1 = read(f1);
+	f2 = read(f2);
+	fraction result = add(f1,f2);
+	result = simplify(result);
+	display(result,f1,f2);
 }
